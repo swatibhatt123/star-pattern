3cf17326e7c15cae9ee832e013f3b2910c136ba4
swatibhatt123 <swatibhatt678@gmail.com>
2025-05-15 12:04:26 +0530

    combine reverse Star pattern program

---
+++ combineReversePattern.java
@@ -0,0 +1,38 @@
+
+package patternProgram;
+ import java.util.Scanner;
+public class combineReversePattern
+{ public static void main(String[] args) 
+    {
+        Scanner sc= new Scanner(System.in); 
+        System.out.println("enter the no.of rows");
+         int num=sc.nextInt(); 
+         for(int i =1 ;i <= num ; i++)
+         { 
+            for(int s =num-1; s>=i; s--)
+            {
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++)
+             { 
+                 System.out.print("*"); 
+            }
+             System.out.println(""); 
+        }
+
+        
+        for (int i=1; i<= num ;i++)
+        {
+             for( int s=1 ;s<=i;s++){
+                System.out.print(" ");
+            }
+
+            for(int j=num-1; j>= i; j--){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+           
+        }
+         sc.close();
+    }
+}
